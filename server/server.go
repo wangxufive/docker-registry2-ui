0b6757b613cf2aaf37d89fc7008650e20ec6737f
wangxufire <wangxufire@gmail.com>
2016-05-12 14:10:28 +0800

    init

---
+++ server/server.go
@@ -0,0 +1,41 @@
+// Golang HTML5 Docker Registry V2 UI Server
+//
+// author wangxufire
+
+package main
+
+import (
+  "net/http"
+  "log"
+  "html/template"
+)
+
+func main() {
+  // When get a request at "/", call `MainPageHandler`
+  http.Handle("/", http.HandlerFunc(MainPageHandler))
+
+  // Start the server and listen forever on port 8000.
+  http.ListenAndServe(":8000", nil)
+}
+
+// Handler for the main page, which we wire up to the
+// route at "/" below in `main`.
+//
+func MainPageHandler(w http.ResponseWriter, r *http.Request) {
+  // insist the path is just "/".
+  if r.URL.Path != "/" {
+    w.WriteHeader(http.StatusNotFound)
+    return
+  }
+
+  t, err := template.ParseFiles("view/index.html")
+  if err != nil {
+    log.Fatal("error parsing your template.")
+  }
+
+  // Render the template, writing to `w`.
+  t.Execute(w, "wangxufire")
+
+  // Done.
+  log.Println("Finished HTTP request at ", r.URL.Path)
+}
