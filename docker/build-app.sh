446eacbd3c3968f03d5273c044908354ae630a35
wangxufire <wangxufire@gmail.com>
2016-05-16 23:29:49 +0800

    add Dockerfile and Makefile

--- docker/build-app.sh
+++ docker/build-app.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Set temp environment vars
+debug "==> Set environment vars for building..."
 export PATH=${PATH}:${GOPATH}/bin
 
-# Init go environment to build Gogs
+debug "==> Init go environment..."
 /bin/mkdir -p ${GOPATH}/src/github.com/wangxufire/
 /bin/ln -s /opt/docker-registry2-ui/ ${GOPATH}/src/github.com/wangxufire/docker-registry2-ui
 cd ${GOPATH}/src/github.com/wangxufire/docker-registry2-ui
 
-/usr/bin/make build
+debug "==> Building..."
+/usr/local/go get -t -v ./...
+/usr/local/go install -v ./...
 
+debug "==> Copy bin to /opt..."
 /bin/cp -a ${GOPATH}/src/github.com/wangxufire/docker-registry2-ui/docker-registry2-ui /opt
 
+debug "==> Cleaning..."
 /bin/rm -rf ${GOPATH}
 
 debug "==> Building server success..."
