75d44d2e893b895102e354df7a40a64eadd0eece
wangxufire <wangxufire@gmail.com>
2016-05-16 17:00:16 +0800

    add Dockerfile and Makefile

--- docker/build-node.sh
+++ docker/build-node.sh
@@ -7,10 +7,9 @@ NPM_VERSION=3.8.6
 && debug "==> Downloading..." \
 && wget -O node.tar.xz http://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz \
 && debug "==> Extracting..." \
-&& tar -xJf node.tar.xz -C /usr --strip-components=1 \
+&& tar -xJf "node.tar.xz" -C /usr --strip-components=1 \
 && debug "==> Cleaning up..." \
-&& /sbin/apk del wget
-
+&& /sbin/apk del wget \
 && /bin/rm -f node.tar.xz \
 && /bin/rm -rf /var/cache/apk/* \
 && debug "==> Building nodejs success..."
